d75d6926b87eb2f85d2dfeb649bdf9e9ad3c3b75
Csaba Salat <csalat@globalblue.com>
2020-04-10 22:35:26 +0200

    scaffolding sqlite in memory db

---
+++ src/Extensions.SQLite.InMemoryDb/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Extensions.SQLite.InMemoryDb
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSqliteInMemoryDb<TContext>(this IServiceCollection services) where TContext : DbContext, new()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            services.AddSingleton(sp => connection);
+            var options = new DbContextOptionsBuilder<TContext>()
+                .UseSqlite(connection)
+                .Options;
+            using var context = CreateContext(options);
+            context.Database.EnsureCreated();
+
+            services.AddSingleton(sp => options);
+            return services;
+        }
+
+        private static TContext CreateContext<TContext>(DbContextOptions<TContext> options) where TContext : DbContext, new() => (TContext)Activator.CreateInstance(typeof(TContext), options);
+    }
+}
